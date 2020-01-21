928ac6aaf5f20af29d013cb83f94f39e500299ba
Brend Smits <brend_smits@hotmail.com>
2020-01-21 23:45:26 +0100

    Welcome command now checks for: - Preventing people from welcoming people they already welcomed before - Only allowing welcoming of new players - Only allowing welcoming of other players - Check if there are new players
    Config is now being generated that allows you to specify commands that are executed when players successfully welcome new players.

--- src/main/java/com/mysticalmachines/WelcomeRecommender.java
+++ src/main/java/com/mysticalmachines/WelcomeRecommender.java
@@ -1,26 +1,35 @@
 package com.mysticalmachines;
 
+import com.google.common.reflect.TypeToken;
 import com.google.inject.Inject;
 import com.mysticalmachines.commands.WelcomeCommand;
+import ninja.leaping.configurate.ConfigurationNode;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.command.args.GenericArguments;
 import org.spongepowered.api.command.spec.CommandSpec;
-import org.spongepowered.api.data.key.Keys;
-import org.spongepowered.api.data.value.mutable.Value;
+import org.spongepowered.api.config.ConfigDir;
+import org.spongepowered.api.config.DefaultConfig;
 import org.spongepowered.api.entity.living.player.Player;
-import org.spongepowered.api.event.game.state.GameInitializationEvent;
-import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.event.network.ClientConnectionEvent;
 import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.text.Text;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Plugin(
         id = "welcomerecommender",
@@ -32,26 +41,55 @@ import java.util.Optional;
 )
 public class WelcomeRecommender {
 
-    public List<Player> newPlayerList = new ArrayList<>();
-
+    public List<String> executeCommandsOnWelcome;
+    public HashMap<Instant, Player> newPlayerMapCache = new HashMap<>();
+    public HashMap<Player, Player> alreadyWelcomedPlayersCache = new HashMap<>();
+    public Player latestPlayer;
     @Inject
     private Logger logger;
-
     @Inject
     private Game game;
+    @Inject
+    private PluginContainer pluginContainer;
+    private ConfigurationNode config;
+    @Inject
+    @ConfigDir(sharedRoot = false)
+    private File configDir;
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    private File defaultConf;
+    @Inject
+    @DefaultConfig(sharedRoot = false)
+    private ConfigurationLoader<CommentedConfigurationNode> loader;
+
+    @Listener
+    public void preInit(GamePreInitializationEvent event) {
+        try {
+            loadConfig();
+            this.executeCommandsOnWelcome = config.getNode("welcomeCommandRewards").getList(TypeToken.of(String.class));
+            for (String command :
+                    this.executeCommandsOnWelcome) {
+                System.out.println("Command: " + command);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Listener
     public void onServerStart(GameStartedServerEvent event) {
         logger.info("Welcome Recommender Started");
+        Task.builder().execute(() -> {
+            deleteOldPlayersFromMap(300);
+        }).async().interval(3, TimeUnit.SECONDS).submit(this);
     }
 
     @Listener
     public void onPlayerFirstJoin(ClientConnectionEvent.Join event) {
         Player player = event.getTargetEntity();
-//        Value<Instant> joinDate = player.firstPlayed();
-//        Value<Instant> lastPlayDate = player.lastPlayed();
         if (!player.hasPlayedBefore()) {
-            newPlayerList.add(player);
+            newPlayerMapCache.put(player.firstPlayed().get(), player);
+            this.latestPlayer = player;
         }
     }
 
@@ -60,23 +98,57 @@ public class WelcomeRecommender {
         createAndRegisterCommands();
     }
 
+
+    private void deleteOldPlayersFromMap(long seconds) {
+        try {
+            if (!this.newPlayerMapCache.isEmpty()) {
+                Iterator<Instant> iterator = this.newPlayerMapCache.keySet().iterator();
+                while (iterator.hasNext()) {
+                    Instant instant = iterator.next();
+                    if (instant.isBefore(Instant.now().minusSeconds(seconds))) {
+                        if (this.newPlayerMapCache.get(instant) == this.latestPlayer) {
+                            this.latestPlayer = null;
+                        }
+                        iterator.remove();
+                    }
+                }
+
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+
+    }
+
     /**
      * Create commands and registers them with the CommandManager.
      */
     private void createAndRegisterCommands() {
 
-        CommandSpec payCommand = CommandSpec.builder()
+        CommandSpec welcomeCommand = CommandSpec.builder()
                 .description(Text.of("Welcome a player"))
-                .permission("welcomerecommender.command.welcome")
                 .executor(new WelcomeCommand(this))
                 .arguments(
                         GenericArguments.optional(
-                                        GenericArguments.user(Text.of("player"))
+                                GenericArguments.player(Text.of("player"))
                         )
                 )
                 .build();
-        game.getCommandManager().register(this, payCommand, "pay");
+        game.getCommandManager().register(this, welcomeCommand, "welcome");
+    }
 
+    /**
+     * Load the default config file, welcomerecommender.conf.
+     */
+    private void loadConfig() {
+        try {
+            if (!defaultConf.exists()) {
+                pluginContainer.getAsset("welcomerecommender.conf").get().copyToFile(defaultConf.toPath());
+            }
 
+            this.config = loader.load();
+        } catch (IOException e) {
+            logger.warn("[WelcomeRecommender] Main configuration file could not be loaded/created/changed!");
+        }
     }
 }
--- src/main/java/com/mysticalmachines/commands/WelcomeCommand.java
+++ src/main/java/com/mysticalmachines/commands/WelcomeCommand.java
@@ -1,26 +1,74 @@
 package com.mysticalmachines.commands;
 
 import com.mysticalmachines.WelcomeRecommender;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.command.CommandException;
 import org.spongepowered.api.command.CommandResult;
 import org.spongepowered.api.command.CommandSource;
 import org.spongepowered.api.command.args.CommandContext;
 import org.spongepowered.api.command.spec.CommandExecutor;
-import org.spongepowered.api.entity.living.player.User;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.channel.MessageChannel;
+import org.spongepowered.api.text.format.TextColors;
 
 import java.util.Optional;
 
 public class WelcomeCommand implements CommandExecutor {
-    private WelcomeRecommender welcomeRecommender;
+    private WelcomeRecommender welcome;
 
-    public WelcomeCommand(WelcomeRecommender welcomeRecommender) {
-        this.welcomeRecommender = welcomeRecommender;
+    public WelcomeCommand(WelcomeRecommender welcome) {
+        this.welcome = welcome;
     }
 
     @Override
     public CommandResult execute(CommandSource src, CommandContext args) throws CommandException {
-        Optional<User> targetPlayer = args.getOne("player");
-//        this.welcomeRecommender.newPlayerList;
-        return null;
+        if (src instanceof Player) {
+            Optional<Player> targetPlayer = args.getOne("player");
+            Player senderPlayer = (Player) src;
+            if (targetPlayer.isPresent() && this.welcome.newPlayerMapCache.containsValue(targetPlayer.get())) {
+                if (isPlayerAllowedToWelcomeTargetPlayer(targetPlayer.get(), senderPlayer)) {
+                    if (targetPlayer.get() == senderPlayer) {
+                        throw new CommandException(Text.of(TextColors.RED, "Tell me, why are you trying to welcome yourself?"));
+                    }
+                    MessageChannel.TO_ALL.send(Text.of("Welcome, " + targetPlayer.get().getName() + ", enjoy your stay!"));
+                    this.executeConfiguredCommands(targetPlayer.get(), senderPlayer);
+                    this.welcome.alreadyWelcomedPlayersCache.put(targetPlayer.get(), senderPlayer);
+                    return CommandResult.success();
+                }
+                throw new CommandException(Text.of(TextColors.RED, "You already welcomed this player"));
+            } else {
+                if (this.welcome.latestPlayer != null && this.welcome.latestPlayer != senderPlayer) {
+                    if (isPlayerAllowedToWelcomeTargetPlayer(this.welcome.latestPlayer, senderPlayer)) {
+                        MessageChannel.TO_ALL.send(Text.of("Welcome, " + this.welcome.latestPlayer.getName() + ", enjoy your stay!"));
+                        this.executeConfiguredCommands(this.welcome.latestPlayer, senderPlayer);
+                        this.welcome.alreadyWelcomedPlayersCache.put(this.welcome.latestPlayer, senderPlayer);
+                        return CommandResult.success();
+                    }
+                    throw new CommandException(Text.of(TextColors.RED, "You already welcomed this player"));
+                } else {
+                    throw new CommandException(Text.of(TextColors.RED, "There are no new players to welcome"));
+                }
+            }
+        } else {
+            throw new CommandException(Text.of(TextColors.RED, "Command can only be executed by a player, not a console, you dummy."));
+        }
+    }
+
+    private void executeConfiguredCommands(Player targetPlayer, Player commandExecutingPlayer) {
+        for (String command :
+                this.welcome.executeCommandsOnWelcome) {
+            Sponge.getCommandManager().process(Sponge.getServer().getConsole(), command
+                    .replace("%targetPlayerName%", targetPlayer.getName())
+                    .replace("%commandExecutingPlayerName%", commandExecutingPlayer.getName()));
+        }
+    }
+
+    private boolean isPlayerAllowedToWelcomeTargetPlayer(Player targetPlayer, Player senderPlayer) {
+        if (!this.welcome.alreadyWelcomedPlayersCache.isEmpty()) {
+            Player returnedPlayer = this.welcome.alreadyWelcomedPlayersCache.get(targetPlayer);
+            return returnedPlayer == null || returnedPlayer != senderPlayer;
+        }
+        return true;
     }
 }
